2fd4db6a29b005e70e506b3b94537a840e757311
Vicente Adolfo Bolea Sanchez <vicentebolea@gmail.com>
2016-01-28 08:56:15 +0900

    Added test files, dl_loader works

---
+++ tests/dl_test.cc
@@ -0,0 +1,12 @@
+#include <common/dl_loader.hh>
+#include <stdexcept>
+
+SUITE(DL) {
+  TEST(BASIC) {
+    DL_loader* ld = nullptr;
+    CHECK_THROW(ld = new DL_loader("myfile", "myfunc"), 
+        std::runtime_error);
+
+    ld->run();
+  }
+}
