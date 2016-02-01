712a224e62f89ae5d5426168ccb73a5c856d2357
Vicente Adolfo Bolea Sanchez <vicente.bolea@gmail.com>
2016-02-01 10:18:45 +0900

    Starting feature branch #13

---
+++ src/network/centralizedtopology.cc
@@ -0,0 +1,20 @@
+#include "centralizedtopology.hh"
+
+namespace eclipse {
+namespace network {
+
+// establish {{{
+bool CentralizedTopology::establish () {
+}
+// }}}
+// close {{{
+bool CentralizedTopology::close () {
+}
+// }}}
+// is_online {{{
+bool CentralizedTopology::is_online () {
+}
+// }}}
+  
+} /* network  */ 
+} /* eclipse  */ 
