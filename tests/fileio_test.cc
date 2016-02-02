5869330757b564a4860bdcd586a6620ea208d8c5
Kibeom Jin <inthound@gmail.com>
2016-02-02 09:05:01 +0900

    first commit

---
+++ tests/fileio_test.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "fs/fileio.hh"
+#include "common/logger.hh"
+
+using namespace eclipse;
+using namespace std;
+
+  TEST(fileio) {
+	Settings setted = Settings().load(); 
+	string logname	= setted.get<string> ("log.name");
+	string logtype	= setted.get<string> ("log.type");
+	Logger* l = Logger::connect(logname	, logtype );
+	//Settings setted; 
+	FileIO fs(l, setted, "test2.txt", "unist", "berkeley", 1);
+	/*
+	fs.open_writefile("test.txt");
+	string msg = "Hello, world!";
+	fs.write_line(&msg);
+	*/
+	fs.open_writefile();
+	string msg = "Hello, world!";
+	fs.write_file(&msg);
+	//fs.read_line(&msg);
+	//fs.read_file(&msg);
+	//cout << msg << endl;
+
+	//fs2.open_writefile("test2.txt");
+	//fs2.write_line(&msg);
+	Logger::disconnect(l);
+  }
