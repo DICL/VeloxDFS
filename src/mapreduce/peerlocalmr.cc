b21ed6e8f9d6fb92ca0bb1dcc03e5fd5eb377646
Vicente Adolfo Bolea Sanchez <vicente.bolea@gmail.com>
2016-02-03 16:13:59 +0900

    added classes skeletons

---
+++ src/mapreduce/peerlocalmr.cc
@@ -0,0 +1,11 @@
+#include "peerlocalmr.hh"
+
+namespace eclipse {
+
+PeerLocalMR::PeerLocalMR (Context& c) : PeerLocal(c) {
+}
+
+PeerLocalMR::~PeerLocalMR () {
+}
+  
+} /* eclipse  */ 
---
+++ src/mapreduce/peerlocalmr.hh
@@ -0,0 +1,14 @@
+#pragma once
+#include "../common/peerlocal.hh"
+
+namespace eclipse {
+
+class PeerLocalMR: public PeerLocal {
+  public:
+    PeerLocalMR (Context&);
+    ~PeerLocalMR ();
+
+  private:
+};
+
+} /* eclipse  */ 
