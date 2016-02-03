9d1e4e4c0ad804d5e3ef264185248661f571b3cc
Kibeom Jin <inthound@gmail.com>
2016-02-03 14:15:20 +0900

    basic file IO class

--- src/fs/fileio.cc
+++ src/fs/fileio.cc
@@ -3,107 +3,43 @@
 using namespace std;
 
 namespace eclipse {
-FileIO::FileIO(Logger* logger, Settings &setted, string file_name, string key, string value, int jobid) {
-	this->logger	= logger;
-	this->file_name = file_name;
-	path			= setted.get<string>("path.scratch") + "/" + file_name;
-	ipath			= setted.get<string>("path.scratch") + "/idata/";
-	this->key		= key;
-	this->value		= value;
-	this->jobid		= jobid;
-}
-
-bool FileIO::open_readfile()
+void FileIO::open_rfile(string file_name)
 {
+	string path = con.settings.get<string>("path.scratch") + "/" + file_name;
 	file.open(path.c_str(), ios::in);
-	if(file.is_open())
-	{
-		logger->info("Can open the file %s for read", file_name.c_str());
-		return true;
-	}
-	logger->error("Can't open the file %s for read", file_name.c_str());
-	return false;
 }
 
-bool FileIO::open_writefile()
+void FileIO::open_wfile(string file_name)
 {
+	string path = con.settings.get<string>("path.scratch") + "/" + file_name;
 	file.open(path.c_str(), ios::out);
-	if(file.is_open())
-	{
-		logger->info("Can open the file %s for write", file_name.c_str());
-		return true;
-	}
-	logger->error("Can't open the file %s for write", file_name.c_str());
-	return false;
 }
 
-bool FileIO::read_file(string *buf)
+void FileIO::open_irfile(string key)
 {
-	if(file.is_open())
-	{
-		buf->assign((istreambuf_iterator<char>(file)), (istreambuf_iterator<char>()));
-		logger->info("Can read the file %s", file_name.c_str());
-		return true;
-	}
-	logger->error("Can't read the file %s", file_name.c_str());
-	return false;
+	string path = con.settings.get<string>("path.idata") + to_string(jobid) + "_" + key;
+	file.open(path.c_str(), ios::in);
 }
 
-bool FileIO::read_line(string *buf)
+void FileIO::open_iwfile(string key)
 {
-	if(file.is_open())
-	{
-		getline(file, *buf);
-		if(file.eof())
-		{
-			return false;
-		}
-		logger->info("Can read line of the file %s", file_name.c_str());
-		return true;
-	}
-	logger->info("Can't read line of the file %s", file_name.c_str());
-	return false;
+	string path = con.settings.get<string>("path.idata") + to_string(jobid) + "_" + key;
+	file.open(path.c_str(), ios::out);
 }
 
-bool FileIO::write_file(const string *buf)
+void FileIO::read_file(string *buf)
 {
-	if(file.is_open())
-	{
-		file << *buf;
-		logger->info("Can write line to the file %s", file_name.c_str());
-		return true;
-	}
-	logger->info("Can't write line to the file %s", file_name.c_str());
-	return false;
+	buf->assign((istreambuf_iterator<char>(file)), (istreambuf_iterator<char>()));
 }
 
-string FileIO::read_inter()
+void FileIO::read_line(string *buf)
 {
-	if(file.is_open())
-	{
-		getline(file, *buf);
-		if(file.eof())
-		{
-			return false;
-		}
-		logger->info("Can read line of the file %s", file_name.c_str());
-		return true;
-	}
-	logger->info("Can't read line of the file %s", file_name.c_str());
-	return false;
-
-	return "TMP";
+	getline(file, *buf);
 }
 
-bool FileIO::write_inter()
+void FileIO::write_file(const string *buf)
 {
-	return true;
+	file << *buf;
 }
-
-string FileIO::display_path()
-{
-	return path;
-}
-
 
 }
--- tests/fileio_test.cc
+++ tests/fileio_test.cc
@@ -1,32 +1,53 @@
 #include <iostream>
 #include "fs/fileio.hh"
-#include "common/logger.hh"
+#include "common/context.hh"
 
 using namespace eclipse;
 using namespace std;
 
   TEST(fileio) {
-	Settings setted = Settings().load(); 
-	string logname	= setted.get<string> ("log.name");
-	string logtype	= setted.get<string> ("log.type");
-	Logger* l = Logger::connect(logname	, logtype );
+	  Context con;
+	//Settings setted = Settings().load(); 
+	//string logname	= setted.get<string> ("log.name");
+	//string logtype	= setted.get<string> ("log.type");
+	//Logger* l = Logger::connect(logname	, logtype );
 	//Settings setted; 
-	FileIO fs(l, setted, "test2.txt", "unist", "berkeley", 1);
+	FileIO fs1(0), fs2(0), fs3(1), fs4(1), fs5(1);
 	/*
 	fs.open_writefile("test.txt");
 	string msg = "Hello, world!";
 	fs.write_line(&msg);
 	*/
-	fs.open_writefile();
-	string msg = "Hello, world!";
-	fs.write_file(&msg);
-	string path = fs.display_path();
-	l->info("path is %s", path.c_str()); 
+	string msg1 = "Good!";
+	string msg2, msg3, msg4;
+
+	fs1.open_wfile("test.txt");
+	fs1.write_file(&msg1);
+	fs1.close_file();
+
+	fs2.open_rfile("test.txt");
+	fs2.read_file(&msg2);
+	con.logger->info("[TEST] msg2 is %s", msg2.c_str());
+	fs2.close_file();
+	
+	fs3.open_iwfile("key1");
+	fs3.write_file(&msg2);
+	fs3.close_file();
+
+	fs4.open_irfile("key1");
+	fs4.read_file(&msg3);
+	con.logger->info("[TEST] msg3 is %s", msg3.c_str());
+	fs4.close_file();
+
+	fs5.open_rfile("test.txt");
+	fs5.read_line(&msg4);
+	con.logger->info("[TEST] msg4 is %s", msg4.c_str());
+	fs5.close_file();
+
 	//fs.read_line(&msg);
 	//fs.read_file(&msg);
 	//cout << msg << endl;
 
 	//fs2.open_writefile("test2.txt");
 	//fs2.write_line(&msg);
-	Logger::disconnect(l);
   }
