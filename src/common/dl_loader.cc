c11a48892bb242fd63a396e33a65188bc31eff72
Vicente Adolfo Bolea Sanchez <vicente.bolea@gmail.com>
2016-02-03 13:41:31 +0900

    Waiting for PeerLocalMR

--- src/common/dl_loader.cc
+++ src/common/dl_loader.cc
@@ -5,26 +5,28 @@
 using namespace std;
 
 // Constructor& destructors {{{
-DL_loader::DL_loader(string path, string fun) {
+DL_loader::DL_loader(string lib, string in):
+  input(in), lib_name(lib){ }
+DL_loader::~DL_loader() {
+  dlclose(lib);
+}
+// }}}
+// init_executor {{{
+bool DL_loader::init_executor (PeerLocalMR*) {
   lib = dlopen(path.c_str(), RTLD_NOW);
 
   if (!lib) throw std::runtime_error("Path not found");
 
   dlerror();
-
-  func_ = reinterpret_cast<void(*)()>(dlsym(lib, fun.c_str())); 
+}
+// }}}
+// load_function {{{
+maptype DL_loader::load_function (std::string) {
+  maptype func_ = 
+    reinterpret_cast<maptype>(dlsym(lib, fun.c_str())); 
   char* err = dlerror();
 
   if (err) throw std::runtime_error("Symbol not found");
-}
-
-DL_loader::~DL_loader() {
-  dlclose(lib);
+  return func_;
 }
 // }}}
-// run {{{
-void DL_loader::run() {
-  this->func_();
-}
-// }}}
-//
