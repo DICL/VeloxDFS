8dda095b42e0520163c271028e66439f72fd4646
Vicente Adolfo Bolea Sanchez <vicente.bolea@gmail.com>
2016-02-03 09:48:28 +0900

    Waiting for refactor-context

--- src/mapreduce/executor.cc
+++ src/mapreduce/executor.cc
@@ -1,4 +1,4 @@
-#include "nodeexecutor.hh"
+#include "executor.hh"
 #include "../messages/factory.hh"
 #include "../messages/boost_impl.hh"
 
@@ -9,50 +9,65 @@ using namespace std;
 
 namespace eclipse {
 
-Executor::Executor() {
+// Constructor {{{
+Executor::Executor(Settings& setted) : MR_traits (setted) {
 
 }
 
 Executor::~Executor() {
 
 }
-// process_message (Job* m) {{{
-template<> void Executor::process_message (Task* m) {
+// }}}
+//// process_message (Job* m) {{{
+//template<> void Executor::process_message (Task* m) {
+//}
+//// }}}
+//// process_message (Control* m) {{{
+//template<> void Executor::process_message (Control* m) {
+//  switch (m->type) {
+//    case messages::SHUTDOWN:
+//      exit(EXIT_SUCCESS);
+//      break;
+//
+//    case messages::RESTART:
+//      break;
+//
+//      //    case PING:
+//      //      process_ping (m);
+//      //      break;
+//  }
+//}
+//// }}}
+//// process_message (Message*) {{{
+//template<> void Executor::process_message (Message* m) {
+//  string type = m->get_type();
+//
+//  if (type == "Job") {
+//    auto m_ = dynamic_cast<Control*>(m);
+//    process_message(m_);
+//
+//  } else if (type == "KeyRequest") {
+//    auto m_ = dynamic_cast<KeyRequest*>(m);
+//    process_message(m_);
+//  }
+//}
+//// }}}
+// action {{{
+void Executor::action (boost::asio::ip::tcp::socket* sock) {
+  do_read();
 }
 // }}}
-// process_message (Control* m) {{{
-template<> void Executor::process_message (Control* m) {
-  switch (m->type) {
-    case messages::SHUTDOWN:
-      exit(EXIT_SUCCESS);
-      break;
+// do_read {{{
+void Executor::do_read () {
 
-    case messages::RESTART:
-      break;
-
-      //    case PING:
-      //      process_ping (m);
-      //      break;
-  }
 }
 // }}}
-// process_message (Message*) {{{
-template<> void Executor::process_message (Message* m) {
-  string type = m->get_type();
 
-  if (type == "Job") {
-    auto m_ = dynamic_cast<Control*>(m);
-    process_message(m_);
+void Executor::on_read_header (const error_code& ec, size_t s) {
 
-  } else if (type == "KeyRequest") {
-    auto m_ = dynamic_cast<KeyRequest*>(m);
-    process_message(m_);
-  }
 }
-// }}}
-// load {{{
-bool Executor::load (std::string) {
+void Executor::on_read_body (const error_code& ec, size_t s) {
+
 }
-// }}}
 
 } /* eclipse  */
--- src/mapreduce/mr_traits.hh
+++ src/mapreduce/mr_traits.hh
@@ -3,19 +3,24 @@
 #include "../common/dl_loader.hh"
 #include "../network/centralizedtopology.hh"
 
-namespace eclipse {
+#include  <string>
+#include  <boost/asio.hpp>
 
+namespace eclipse {
+namespace network {
+class CentralizedTopology;
+}
 class MR_traits: public NodeLocal {
   public:
     MR_traits(Settings&);
     ~MR_traits();
 
-    virtual bool load(std::string) = 0;
-    bool establish ();
+    bool establish () override;
+    virtual void action (boost::asio::ip::tcp::socket*) = 0;
 
   protected:
     DL_loader* dl_loader;
-    unique_prt<CentralizedTopology> network;
+    u_ptr<eclipse::network::CentralizedTopology> network;
 };
 
 }
