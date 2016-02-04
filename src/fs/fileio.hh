c25b7b4d74e9ffb6be1a33cf0cafb648978eb65e
Kibeom Jin <inthound@gmail.com>
2016-02-04 22:53:23 +0900

    multi block dealing

--- src/fs/fileio.hh
+++ src/fs/fileio.hh
@@ -1,28 +1,29 @@
 #pragma once
 #include "../common/context.hh"
+#include "fileinfo.hh"
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <map>
 
 namespace eclipse {
-class FileIO {
-	private:
-		Context con;
-		int jobid;
-		std::fstream file;
+  class FileIO {
+    private:
+      Context con;
+      FileInfo f_info;
+      std::fstream file;
+      std::map<std::string, FileInfo> f_map;
 
-	public:
-		FileIO(int);
-		~FileIO();
-
-		void open_rfile(std::string);	// open file for read
-		void open_wfile(std::string);	// open file for write
-		void open_irfile(std::string);	// open intermediate file for read
-		void open_iwfile(std::string);	// open intermediate file for write 
-		void read_file(std::string *);	// read whole file
-		void read_line(std::string *);	// read one line
-		void write_file(const std::string *);	// write string
-		void close_file();				// close file
-};
-
+    public:
+      FileIO();
+      ~FileIO();
+      void open_read(int, std::string, bool);  // open file for read
+      void open_write(int, std::string, bool);  // open file for write
+      void read_file(std::string *);            // read whole file
+      void read_idata(std::string *);            // read one line
+      void write_file(const std::string *);     // write string
+      void write_idata(const std::string *);     // write string
+      void close_file();                        // close file
+      bool is_end();                            // check eof
+  };
 }
--- tests/fileio_test.cc
+++ tests/fileio_test.cc
@@ -6,48 +6,28 @@ using namespace eclipse;
 using namespace std;
 
   TEST(fileio) {
-	  Context con;
-	//Settings setted = Settings().load(); 
-	//string logname	= setted.get<string> ("log.name");
-	//string logtype	= setted.get<string> ("log.type");
-	//Logger* l = Logger::connect(logname	, logtype );
-	//Settings setted; 
-	FileIO fs1(0), fs2(0), fs3(1), fs4(1), fs5(1);
-	/*
-	fs.open_writefile("test.txt");
-	string msg = "Hello, world!";
-	fs.write_line(&msg);
-	*/
+    Context con;
+	FileIO fs1;
 	string msg1 = "Good!";
-	string msg2, msg3, msg4;
+  string msg3 = "Great!";
+	string msg2, msg4;
 
-	fs1.open_wfile("test.txt");
+	fs1.open_write(0, "test.txt", true);
 	fs1.write_file(&msg1);
 	fs1.close_file();
 
-	fs2.open_rfile("test.txt");
-	fs2.read_file(&msg2);
+	fs1.open_read(1, "test.txt", true);
+	fs1.read_file(&msg2);
 	con.logger->info("[TEST] msg2 is %s", msg2.c_str());
-	fs2.close_file();
-	
-	fs3.open_iwfile("key1");
-	fs3.write_file(&msg2);
-	fs3.close_file();
+	fs1.close_file();
 
-	fs4.open_irfile("key1");
-	fs4.read_file(&msg3);
-	con.logger->info("[TEST] msg3 is %s", msg3.c_str());
-	fs4.close_file();
+	fs1.open_write(2, "key1", false);
+	fs1.write_idata(&msg3);
+	fs1.close_file();
 
-	fs5.open_rfile("test.txt");
-	fs5.read_line(&msg4);
+	fs1.open_read(2, "key1", false);
+	fs1.read_idata(&msg4);
 	con.logger->info("[TEST] msg4 is %s", msg4.c_str());
-	fs5.close_file();
+	fs1.close_file();
 
-	//fs.read_line(&msg);
-	//fs.read_file(&msg);
-	//cout << msg << endl;
-
-	//fs2.open_writefile("test2.txt");
-	//fs2.write_line(&msg);
   }
