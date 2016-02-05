317a93d5f379b0fd16fb19f4ed501c7b7e787097
Kibeom Jin <inthound@gmail.com>
2016-02-05 16:37:35 +0900

    first succeeded version

--- src/fs/fileio.cc
+++ src/fs/fileio.cc
@@ -19,48 +19,56 @@ namespace eclipse {
     if(type == true) // input or output file
     {
       path = con.settings.get<string>("path.scratch") + "/" + name;
+      auto search = f_map.find(path);
+      if(search != f_map.end())
+      {
+        *f_info = search->second;
+        file.open(path.c_str(), ios::in);
+      }
+      else
+      {
+        con.logger->error("[fileIO] Job %d: open_r %s failed.", jobid, name.c_str());
+      }
     }
     else // intermediate data
     {
       path = con.settings.get<string>("path.idata") + "/" + to_string(jobid) + "_" + name;
-    }
-    auto search = f_map.find(path);
-    if(search != f_map.end())
-    {
-      f_info.jobid = search.jobid;
-      f_info.num_block = search.num_block;
-      f_info.cur_block = search.cur_block;
-      f_info.size = search.size;
-      f_info.path = search.path;
-      f_info.type = search.type;
-      file.open(path.c_str(), ios::in);
-    }
-    else
-    {
-      con.logger->error("[fileIO] Job %d: open_r %s failed.", jobid, name.c_str());
+      auto search = f_map.find(path);
+      if(search != f_map.end())
+      {
+        *f_info = search->second;
+        path = path + "_" + to_string(f_info->cur_block);
+        file.open(path.c_str(), ios::in);
+      }
+      else
+      {
+        con.logger->error("[fileIO] Job %d: open_r %s failed.", jobid, name.c_str());
+      }
     }
   }
 
   void FileIO::open_write(int jobid, string name, bool type)
-  {
-    //f_info = new FileInfo;
-    f_info.jobid = jobid;
-    f_info.num_block = 1;
-    f_info.cur_block = 1;
-    f_info.type = type;
-    f_info.size = 0;
+  { 
+    FileInfo tmp_info;
+    tmp_info.jobid = jobid;
+    tmp_info.num_block = 1;
+    tmp_info.cur_block = 1;
+    tmp_info.type = type;
+    tmp_info.size = 0;
     if(type == true) // input or output file
     {
-      f_info.path = con.settings.get<string>("path.scratch") + "/" + name;
-      file.open(f_info.path.c_str(), ios::out | ios::app);
+      tmp_info.path = con.settings.get<string>("path.scratch") + "/" + name;
+      file.open(tmp_info.path.c_str(), ios::out | ios::app);
     }
     else // intermediate data
     {
-      f_info.path = con.settings.get<string>("path.idata") + "/" + to_string(jobid) + "_" + name;
-      string path = f_info.path + "_" + to_string(f_info.num_block);
+      tmp_info.path = con.settings.get<string>("path.idata") + "/" + to_string(jobid) + "_" + name;
+      string path = tmp_info.path + "_" + to_string(tmp_info.num_block);
       file.open(path.c_str(), ios::out | ios::app);
     }
-    f_map.insert(pair<string, FileInfo>(f_info.path, f_info));
+    f_map.insert(pair<string, FileInfo>(tmp_info.path, tmp_info));
+    auto search = f_map.find(tmp_info.path);
+    f_info = &(search->second);
   }
 
   void FileIO::read_file(string *buf)
@@ -73,12 +81,12 @@ namespace eclipse {
     getline(file, *buf);
     if(file.eof())
     {
-      f_info.cur_block++;
-      if(f_info.cur_block < f_info.num_block)
+      f_info->cur_block++;
+      if(f_info->cur_block < f_info->num_block)
       {
         file.close();
-        string path = f_info.path + "_" + to_string(f_info.cur_block + 1);
-        file.open(path.c_str(), ios::out | ios::app);
+        string path = f_info->path + "_" + to_string(f_info->cur_block + 1);
+        file.open(path.c_str(), ios::in | ios::app);
       }
     }
   }
@@ -91,27 +99,37 @@ namespace eclipse {
   void FileIO::write_idata(const string *buf)
   {
     size_t limit = con.settings.get<int>("block.size");
-    if(f_info.size + buf->size() > limit)
+     // con.logger->info("size of size = %d", f_info->size);
+    //string tmp = *buf;
+      //con.logger->info("size of tmp = %d", tmp.length());
+      //con.logger->info("tmp = %s", tmp.c_str());
+
+    
+    if(f_info->size + buf->length() > limit)
     {
+      //con.logger->info("OVER!!!!");
       file.close();
-      string path = f_info.path + "_" + to_string(++f_info.num_block);
+      string path = f_info->path + "_" + to_string(++(f_info->num_block));
       file.open(path.c_str(), ios::out | ios::app);
+      f_info->size = 0;
     }      
     file << *buf;
+    f_info->size += buf->length();
+      //con.logger->info("size of tmp = %d", tmp.length());
+      //con.logger->info("size of size = %d", f_info->size);
   }
 
   void FileIO::close_file()
   {
     if(file.is_open())
     {
-      auto search = f_map.find(path);
-      if(search != f_map.end())
       file.close();
+    }
   }
 
   bool FileIO::is_end()
   {
-    if(f_info.cur_block < f_info.num_block)
+    if(f_info->cur_block < f_info->num_block)
     {
       return false;
     }
