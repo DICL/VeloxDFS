3278834bd3bc9da6f7667c7846746bc535a7f797
Vicente Adolfo Bolea Sanchez <vicente.bolea@gmail.com>
2016-02-06 13:42:59 +0900

    it compiles

--- src/common/dl_loader.cc
+++ src/common/dl_loader.cc
@@ -5,23 +5,23 @@
 using namespace std;
 
 // Constructor& destructors {{{
-DL_loader::DL_loader(string lib, string in):
-  input(in), lib_name(lib){ }
+DL_loader::DL_loader(string lib):
+ lib_name(lib){ }
 DL_loader::~DL_loader() {
-  dlclose(lib);
 }
 // }}}
 // init_executor {{{
-bool DL_loader::init_executor (PeerLocalMR*) {
-  lib = dlopen(path.c_str(), RTLD_NOW);
+bool DL_loader::init_lib () {
+  lib = dlopen(lib_name.c_str(), RTLD_NOW);
 
   if (!lib) throw std::runtime_error("Path not found");
 
   dlerror();
+  return true;
 }
 // }}}
 // load_function {{{
-maptype DL_loader::load_function (std::string) {
+maptype DL_loader::load_function (std::string fun) {
   maptype func_ = 
     reinterpret_cast<maptype>(dlsym(lib, fun.c_str())); 
   char* err = dlerror();
@@ -30,3 +30,6 @@ maptype DL_loader::load_function (std::string) {
   return func_;
 }
 // }}}
+void DL_loader::close() {
+  dlclose(lib);
+}
--- src/mapreduce/executor.hh
+++ src/mapreduce/executor.hh
@@ -1,5 +1,7 @@
 #pragma once
 #include "mr_traits.hh"
+#include "peerlocalmr.hh"
+#include "../messages/task.hh"
 
 namespace eclipse {
 
@@ -15,16 +17,19 @@ class Executor: public MR_traits {
     void action (tcp::socket*) override;
 
   protected:
-    template <typename T> void process_message (T);
+    PeerLocalMR peer_cache;
     boost::asio::streambuf inbound_data;
     char inbound_header [16];
     const int header_size = 16;
 
-    void do_read(tcp::socket* sock);
+    template <typename T> void process_message (T);
+    void do_read (tcp::socket* sock);
     void on_read_header (const error_code&, size_t, 
         tcp::socket*); 
     void on_read_body (const error_code&, size_t, 
         tcp::socket*); 
+
+    void run_map (messages::Task*, std::string);
 };
 
 } /* eclipse  */ 
