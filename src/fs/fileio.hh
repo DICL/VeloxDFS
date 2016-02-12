b0adc11fcfa492a2779e8f8acbeda7511f2e9ca3
Kibeom Jin <inthound@gmail.com>
2016-02-12 13:07:27 +0900

    sqlite added (incomplete)

--- src/fs/fileio.hh
+++ src/fs/fileio.hh
@@ -5,25 +5,34 @@
 #include <fstream>
 #include <string>
 #include <map>
+#include <sqlite3>
+#include <sstream>
 
 namespace eclipse {
   class FileIO {
     private:
       Context con;
-      FileInfo *f_info;
       std::fstream file;
-      std::map<std::string, FileInfo> f_map;
+      sqlite3 *db;
+      char *zErrMsg;
+      int rc;
+      vector<BlockInfo> block_info;
 
     public:
       FileIO();
       ~FileIO();
-      void open_read(int, std::string, bool);  // open file for read
-      void open_write(int, std::string, bool);  // open file for write
-      void read_file(std::string *);            // read whole file
+      void init_db();
+      void open_write(std::string);  // open file for write
+      void open_read(std::string);  // open file for read
+      void open_iwrite(int, std::string);  // open file for write
+      void open_iread(int, std::string);  // open file for read
       void write_file(const std::string *);     // write string
-      void read_idata(std::string *);            // read one line
+      void read_file(std::string *);            // read whole file
       void write_idata(const std::string *);     // write string
+      void read_idata(std::string *);            // read one line
       void close_file();                        // close file
-      bool is_end();                            // check eof
+      bool is_remain();                            // check eof
+      void remove_file(std::string);
+      void clear();
   };
 }
