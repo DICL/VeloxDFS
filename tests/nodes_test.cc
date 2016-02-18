28bc6de0ccf9f49254bb4441f1e23e6af9637bc8
Vicente Adolfo Bolea Sanchez <vicentebolea@gmail.com>
2016-02-18 11:40:15 +0900

    Fixed unit tests

--- tests/nodes_test.cc
+++ tests/nodes_test.cc
@@ -1,19 +1,16 @@
-#include <nodes/peerlocal.hh>
-#include <nodes/noderemote.hh>
+#include <nodes/peer.hh>
+#include <common/context.hh>
 
 using namespace eclipse;
 
 SUITE(NODES) {
   TEST(BASIC) {
-    //Node n;
-    
     std::string input (
 "{\"name\":\"Raven cluster eclipseMR file\",\"max_job\":1024,\"log\":{\"type\":\"LOG_LOCAL6\",\"name\":\"ECLIPSE\"},\"path\":{\"filesystem\":\"/home/vicente/eclipse_storage/\",\"scratch\":\"/scratch/vicente/storage\",\"ipc\":\"/scratch/vicente/socket\"},\"cache\":{\"numbin\":100,\"size\":200000,\"concurrency\":1},\"network\":{\"port_mapreduce\":8008,\"port_cache\":8009,\"port_dht\":8010,\"topology\":\"mesh\",\"iface\":\"em1\",\"master\":\"10.20.13.123\",\"nodes\":[\"10.20.13.123\",\"10.20.13.124\",\"10.20.13.125\"]}}");
     Context context(input);
-    PeerLocal nl (context);
+    Peer nl (context);
 
     sleep (1);
-    //NodeRemote nr (io, "localhost", 10, 0);
   }
 }
 
