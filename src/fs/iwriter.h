047bbe5e8bd0056e334ed4f7f454c86a4640dc98
Wonbae Kim <zwigul@gmail.com>
2016-02-25 10:22:33 +0900

    Set hash function as round-robin

--- src/fs/iwriter.h
+++ src/fs/iwriter.h
@@ -2,12 +2,14 @@
 #define SRC_FS_IWRITER_H_
 #include <string>
 #include <map>
+#include <unordered_map>
 #include <vector>
 #include <list>
 #include "../common/context.hh"
 
 using std::vector;
 using std::multimap;
+using std::unordered_map;
 using std::string;
 
 namespace eclipse {
@@ -17,18 +19,21 @@ class IWriter {
   IWriter();
   ~IWriter();
 
-  void AddKeyValue(string key, string value);
+  void AddKeyValue(const string &key, const string &value);
 
  private:
-  void Flush(int index);
-  int get_block_size(int index);
-  void set_block_size(int index, int size);
+  void Flush(const int &index);
+  int get_block_size(const int &index);
+  void set_block_size(const int &index, const int &size);
+  int round_robin(const string &key);
 
   Context con;
   int reduce_slot_;
   int iblock_size_;
+  int index_counter_;
   vector<multimap<string, string>*> kmv_blocks_;
   vector<int> block_size_;
+  unordered_map<string, int> key_index_;
 };
 
 }  // namespace eclipse
