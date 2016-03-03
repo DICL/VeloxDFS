554f394a4832a93d2a9653e38f01c82eaa328998
Kibeom Jin <inthound@gmail.com>
2016-03-03 13:13:37 +0900

    fist step

--- src/fs/dfsput.cc
+++ src/fs/dfsput.cc
@@ -1,34 +1,38 @@
 #include <iostream>
-#include <string.h>
-#include <fstream>
-#include <string>
-#include "../common/hash.hh"
-#include "../common/context.hh"
-#include "fileinfo.hh"
-#include "blockinfo.hh"
-#include "directory.hh"
+//#include <string.h>
+//#include <fstream>
+//#include <string>
+//#include "common/hash.hh"
+//#include "common/context.hh"
+//#include "fileinfo.hh"
+//#include "blockinfo.hh"
+//#include "directory.hh"
 
 using namespace std;
-using namespace eclipse;
+//using namespace eclipse;
 
 int main(int argc, char* argv[])
 {
-  Context con;
-  Directory dir;
+//  Context con;
+//  Directory dir;
+/*  
   if(argc < 2)
   {
     cout << "usage: dfsput file_name1 file_name2 ..." << endl;
     return -1;
   }
-  else
-  {
-    uint32_t BLOCK_SIZE = con.settings.get<int>("filesystem.block");
-    uint32_t NUM_SERVERS = con.settings.get<vector<string>>("network.nodes").size();
+*/  
+//  else
+//  {
+/*
+cout << "step 0" << endl;
+    const uint32_t BLOCK_SIZE = con.settings.get<int>("filesystem.block");
+    const uint32_t NUM_SERVERS = con.settings.get<vector<string>>("network.nodes").size();
     string path = con.settings.get<string>("path.scratch");
-    //file_info.replica = con.settings.get<int>("filesystem.replica");
+    char *buff = new char[BLOCK_SIZE];
+cout << "step 1" << endl;
     for(int i=1; i<argc; i++)
     {
-      char *buff = new char[BLOCK_SIZE];
       bzero(buff, BLOCK_SIZE);
       ifstream myfile;
       string file_name = argv[i];
@@ -36,24 +40,19 @@ int main(int argc, char* argv[])
       myfile.seekg(0, myfile.end);
       uint64_t file_size = myfile.tellg();
       unsigned int block_seq = 0;
-      uint32_t start = 0;
-      uint32_t end = start + BLOCK_SIZE - 1;
-      uint32_t file_hash_key = h (file_name);
-
+      uint64_t start = 0;
+      uint64_t end = start + BLOCK_SIZE - 1;
+      uint32_t file_hash_key = h(file_name);
       //TODO: remote_metadata_server = lookup(hkey);
-      int remote_metadata_server = 1;
-
+      //int remote_metadata_server = 1;
       uint32_t file_id;
       while(1)
       {
-        file_id = h(file_name);
+        file_id = h(file_name); // file_id = h(rand());
 
         // TODO: if(!remote_metadata_server.is_exist(file_id))
         if(1)
-
-        {
           break;
-        }
       }
       FileInfo file_info;
       file_info.file_id = file_id;
@@ -61,20 +60,17 @@ int main(int argc, char* argv[])
       file_info.file_hash_key = file_hash_key;
       file_info.file_size = file_size;
       file_info.num_block = block_seq;
+      file_info.replica = con.settings.get<int>("filesystem.replica");
 
-      // PSEUDO CODE
       // TODO: remote_metadata_server.insert_file_metadata(file_info);
       //cout << "remote_metadata_server.insert_file_metadata(file_info);" << endl;
-
       // this function should call FileIO.open_file() in remote metadata server;
-
 
       while(1)
       {
-        
-        if(end < file_size){
+        if(end < file_size)
+        {
           myfile.seekg(start+BLOCK_SIZE-1, myfile.beg);
-
           while(1)
           {
             if(myfile.peek() =='\n') break;
@@ -91,9 +87,8 @@ int main(int argc, char* argv[])
           end = start + BLOCK_SIZE - 1;
 
           unsigned int block_hash_key = rand()%NUM_SERVERS;
-
           //TODO: int remote_server = lookup(block_hash_key);
-          int remote_server = 1;
+          //int remote_server = 1;
 
           BlockInfo block_info;
           block_info.file_id = file_id;
@@ -102,12 +97,14 @@ int main(int argc, char* argv[])
           block_info.block_name = file_info.file_name + "_" + to_string(block_seq++);
           block_info.block_size = block_size;
           block_info.is_inter = 0;
+          block_info.node = "1.1.1.1";
+          block_info.l_node = "1.1.1.0";
+          block_info.r_node = "1.1.1.2";
           //block_info.node = remote_server.ip_address;
-          //l_server = lookup((block_hash_key-1+NUM_SERVERS)%NUM_SERVERS);
-          //r_server = lookup((block_hash_key+1+NUM_SERVERS)%NUM_SERVERS);
-          //block_info.l_node = l_server.ip_address;
-          //block_info.r_node = r_server.ip_address;
-          block_info.is_commit = 1;
+          //Node l_node = lookup((block_hash_key-1+NUM_SERVERS)%NUM_SERVERS);
+          //Node r_node = lookup((block_hash_key+1+NUM_SERVERS)%NUM_SERVERS);
+          //block_info.l_node = l_node.ip_address;
+          //block_info.r_node = r_node.ip_address;
           file_info.num_block = block_seq;
 
           //TODO: remote_metadata_server.update_file_metadata(fileinfo.file_id, file_info);
@@ -127,7 +124,8 @@ int main(int argc, char* argv[])
           block << buff;
           block.close();
         }
-        else{  // last block
+        else // last block
+        {
           myfile.seekg(start, myfile.beg);
           myfile.read(buff, file_size-start);
           uint32_t block_size = end - start;
@@ -145,13 +143,16 @@ int main(int argc, char* argv[])
           block_info.block_name = file_name + "_" + to_string(block_seq++);
           block_info.block_size = block_size;
           block_info.is_inter = 0;
+          block_info.node = "1.1.1.1";
+          block_info.l_node = "1.1.1.0";
+          block_info.r_node = "1.1.1.2";
           //block_info.node = remote_server.ip_address;
-          //l_server = lookup(block_hash_key-1);
-          //r_server = lookup(block_hash_key+1);
-          //block_info.l_node = l_server.ip_address;
-          //block_info.r_node = r_server.ip_address;
-          block_info.is_commit = 1;
+          //Node l_node = lookup((block_hash_key-1+NUM_SERVERS)%NUM_SERVERS);
+          //Node r_node = lookup((block_hash_key+1+NUM_SERVERS)%NUM_SERVERS);
+          //block_info.l_node = l_node.ip_address;
+          //block_info.r_node = r_node.ip_address;
           file_info.num_block = block_seq;
+
 
           // TODO: remote_metadata_server.update_file_metadata(fileinfo.file_id, file_info);
           //cout << "remote_metadata_server.update_file_metadata(fileinfo.file_id, file_info);" << endl;
@@ -173,9 +174,9 @@ int main(int argc, char* argv[])
         }
       }
       myfile.close();
-      delete[] buff;
     }
-
-  }
-  return 0;
+    delete[] buff;
+*/
+//  }
+//  return 0;
 }
