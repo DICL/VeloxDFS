fdb6a57f01001a7202912d25f10208152f9e27d1
Travis <Travis>
2016-06-17 08:05:00 +0000

    API:

--- search/all_6f.js
+++ search/all_6f.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['offsetkeyvalue',['OffsetKeyValue',['../structeclipse_1_1messages_1_1OffsetKeyValue.html',1,'eclipse::messages']]],
   ['operator_3d',['operator=',['../classSettings.html#a6f7ebd270d9257b41358bc2897bdc2a6',1,'Settings']]]
 ];
---
+++ search/classes_6f.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['offsetkeyvalue',['OffsetKeyValue',['../structeclipse_1_1messages_1_1OffsetKeyValue.html',1,'eclipse::messages']]]
+];
