57a04efdfa4191b16cd9a0cd3f60ffbf226af09f
Vicente Adolfo Bolea Sanchez <vicente.bolea@gmail.com>
2018-10-11 16:20:18 +0900

    adds BASH veloxd, replacement of eclipsed (#145)
    It comes from integrating-with-hadoop branch

--- src/targets/velox_get_config.cc
+++ src/targets/velox_get_config.cc
@@ -5,11 +5,21 @@
 using namespace std;
 
 int main(int argc, char** argv) {
-  string query = argv[1];
+  string query = argv[1]; // Get query
+
+  // Get option
+  string opt; 
+  if (argc > 2) {
+   opt = argv[2];
+  }
 
   if (query == "id") {
     cout << context.id << endl;
 
+  } else if (opt == "-v" ) {
+    for (auto elem : GET_VEC_STR(query)) {
+      cout << elem << endl;
+    }
   } else {
     cout << GET_STR(argv[1]) << endl;
   }
